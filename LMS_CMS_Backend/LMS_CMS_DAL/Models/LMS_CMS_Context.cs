d8ce62d0e5f60c071be03168ee1e6e5d3ee3d962
= <=>
2024-10-21 12:15:40 +0300

    Backend Creation

---
+++ LMS_CMS_Backend/LMS_CMS_DAL/Models/LMS_CMS_Context.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS_CMS_DAL.Models
+{
+    public partial class LMS_CMS_Context : DbContext
+    {
+        public LMS_CMS_Context()
+        {
+        }
+
+        public LMS_CMS_Context(DbContextOptions<LMS_CMS_Context> options)
+            : base(options)
+        {
+        }
+    }
+}

1c62ab927f4371565e7e97443c31e64b122a7643
= <=>
2024-10-23 12:56:31 +0300

    test

---
+++ LMS_CMS_Frontend/src/app/Services/test.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TestService {
+
+  baseurl:string = "";
+
+  constructor(public Api:ApiService, private http: HttpClient) {
+    this.baseurl=Api.BaseUrl
+  }
+
+  getData() {
+    return this.http.get<any>(`${this.baseurl}WeatherForecast`);
+  }
+}
--- LMS_CMS_Frontend/src/app/app.component.ts
+++ LMS_CMS_Frontend/src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TestService } from './Services/test.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'LMS_CMS_Frontend';
+  data = null
+
+  constructor(private testService: TestService) { }
+
+  ngOnInit(): void {
+    this.getData()
+  }
+
+  getData(): void {
+    this.testService.getData().subscribe(
+      data => {
+        this.data = data
+        console.log(data)
+    });
+  }
 }

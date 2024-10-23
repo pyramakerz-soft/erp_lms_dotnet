7f38fdbcb8e31fd58381dab42e5c3a29d983a2c7
= <=>
2024-10-23 12:31:52 +0300

    api Service

---
+++ LMS_CMS_Frontend/src/app/Services/api.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  // BaseUrl="http://localhost:7114/" 
+  BaseUrl="http://3.214.27.32:5000/"
+
+  constructor() { }
+}

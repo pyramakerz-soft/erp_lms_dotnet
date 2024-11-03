b057147472300df47d9f17a8d9a0bfec51946f6a
mennausama <mennausama1682@gmail.com>
2024-11-03 11:54:01 +0200

    employee contriller

--- LMS_CMS_Backend/LMS_CMS/Controllers/EmployeeController.cs
+++ LMS_CMS_Backend/LMS_CMS/Controllers/EmployeeController.cs
@@ -71,54 +71,70 @@ namespace LMS_CMS_PL.Controllers
         //    return Ok(employeeDTO);
         //}
 
-        //[HttpGet("Employee_With_Role_Permission/{empID}")]
-        //public async Task<IActionResult> Employee_With_Role_Permission(int empID)
-        //{
-        //    var employee = await _context.Employees
-        //        .Include(e => e.Employee_Roles)
-        //            .ThenInclude(er => er.Role)
-        //                .ThenInclude(r => r.Role_Permissions)
-        //                    .ThenInclude(rdp => rdp.Master_Detailes_Permissions)
-        //                        .ThenInclude(dp => dp.Master_Permission)
-        //                            .ThenInclude(d => d.Modules_Master_permissions)
-        //                              .ThenInclude(p => p.Module)
+        [HttpGet("Employee_With_Role_Permission/{empID}")]
+        public async Task<IActionResult> Employee_With_Role_Permission(int empID)
+        {
+            var employee = await _context.Employees
+                .Include(e => e.Employee_Roles)
+                    .ThenInclude(er => er.Role)
+                        .ThenInclude(r => r.Role_Permissions)
+                            .ThenInclude(rp => rp.Master_Detailes_Permissions)
+                                .ThenInclude(mdp => mdp.Master_Permission)
+                                    .ThenInclude(mp => mp.Modules_Master_permissions)
+                                        .ThenInclude(mmp => mmp.Module)
+                .Include(e => e.Employee_Roles)
+                    .ThenInclude(er => er.Role)
+                        .ThenInclude(r => r.Role_Permissions)
+                            .ThenInclude(rp => rp.Master_Detailes_Permissions)
+                                .ThenInclude(mdp => mdp.Detailed_Permission)
+                .FirstOrDefaultAsync(e => e.ID == empID);
 
+            if (employee == null)
+            {
+                return NotFound();
+            }
 
+            var result = new
+            {
+                Employee = new
+                {
+                    employee.ID,
+                    employee.User_Name,
+                    employee.Email,
+                    Roles = employee.Employee_Roles.Select(er => new
+                    {
+                        er.Role.ID,
+                        er.Role.Name,
+                        Permissions = er.Role.Role_Permissions.Select(rp => new
+                        {
+                            rp.Master_Detailes_Permissions.ID,
+                            MasterPermission = new
+                            {
+                                rp.Master_Detailes_Permissions.Master_Permission.ID,
+                                rp.Master_Detailes_Permissions.Master_Permission.Name,
+                                Modules = rp.Master_Detailes_Permissions.Master_Permission.Modules_Master_permissions.Select(mmp => new
+                                {
+                                    mmp.Module.ID,
+                                    mmp.Module.Name
+                                })
+                            }
+                        }),
+                        DetailedPermissions = er.Role.Role_Permissions.Select(rp => new
+                        {
+                            rp.Master_Detailes_Permissions.Detailed_Permission.ID,
+                            rp.Master_Detailes_Permissions.Detailed_Permission.Name,
+                            MasterPermission = new
+                            {
+                                rp.Master_Detailes_Permissions.Master_Permission.ID,
+                                rp.Master_Detailes_Permissions.Master_Permission.Name
+                            }
+                        }).Distinct()
+                    })
+                }
+            };
 
-        //        .FirstOrDefaultAsync(e => e.ID == empID);
-
-        //    if (employee == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var result = new
-        //    {
-        //        Employee = new
-        //        {
-        //            employee.ID,
-        //            employee.User_Name,
-        //            employee.Email,
-        //            Roles = employee.Employee_Roles.Select(er => new
-        //            {
-        //                er.Role.ID,
-        //                er.Role.Name,
-        //                DetailedPermissions = er.Role.Role_Detailed_Permissions.Select(rdp => new
-        //                {
-        //                    rdp.Detailed_Permissions.ID,
-        //                    rdp.Detailed_Permissions.Name,
-        //                    MasterPermission = new
-        //                    {
-        //                        rdp.Detailed_Permissions.Master_Permissions.ID,
-        //                        rdp.Detailed_Permissions.Master_Permissions.Name
-        //                    }
-        //                })
-        //            })
-        //        }
-        //    };
-
-        //    return Ok(result);
-        //}
+            return Ok(result);
+        }
 
     }
 }

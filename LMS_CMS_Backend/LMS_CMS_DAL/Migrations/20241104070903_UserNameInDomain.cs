900a9452e1d194a73ee9ae8ada286b100f64e228
mennausama <mennausama1682@gmail.com>
2024-11-04 09:13:50 +0200

    userName in domain

---
+++ LMS_CMS_Backend/LMS_CMS_DAL/Migrations/20241104070903_UserNameInDomain.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS_CMS_DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserNameInDomain : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Domains",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "User_Name",
+                table: "Domains",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Domains");
+
+            migrationBuilder.DropColumn(
+                name: "User_Name",
+                table: "Domains");
+        }
+    }
+}
--- LMS_CMS_Backend/LMS_CMS_DAL/Models/Domain.cs
+++ LMS_CMS_Backend/LMS_CMS_DAL/Models/Domain.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,13 @@ namespace LMS_CMS_DAL.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "User_Name is required")]
+        [StringLength(100, ErrorMessage = "Username cannot be longer than 100 characters.")]
+        public string User_Name { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 100 characters.")]
+        public string Password { get; set; }
 
         public ICollection<School> Schools { get; set; } = new HashSet<School>();
 

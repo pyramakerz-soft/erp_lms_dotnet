eb9f6954610103ffe596e5d18ce90303ad630878
mennausama <mennausama1682@gmail.com>
2024-11-07 13:14:40 +0200

    for pull

---
+++ LMS_CMS_Backend/LMS_CMS_DAL/Models/AuditableEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS_CMS_DAL.Models
+{
+    public class AuditableEntity
+    {
+        public int? UpdatedByUserId { get; set; }
+        public Employee? UpdatedByUser { get; set; }
+
+        public int? DeletedByUserId { get; set; }
+        public Employee? DeletedByUser { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}

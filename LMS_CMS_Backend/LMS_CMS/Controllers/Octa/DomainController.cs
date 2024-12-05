f8cd91b1c9241ae7fff20ec3c865a42dd141b430
= <=>
2024-12-05 10:48:41 +0200

    auth in domain controller

--- LMS_CMS_Backend/LMS_CMS/Controllers/Octa/DomainController.cs
+++ LMS_CMS_Backend/LMS_CMS/Controllers/Octa/DomainController.cs
@@ -15,6 +15,7 @@ namespace LMS_CMS_PL.Controllers.Octa
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class DomainController : ControllerBase
     {
         private readonly UOW _Unit_Of_Work;
@@ -29,17 +30,17 @@ namespace LMS_CMS_PL.Controllers.Octa
         [HttpGet]
         public IActionResult Get()
         {
-            //var userClaims = HttpContext.User.Claims;
-            //var userTypeClaim = HttpContext.User.Claims.FirstOrDefault(c => c.Type == "type")?.Value;
-            //if (userTypeClaim == null)
-            //{
-            //    return Unauthorized("User Type claim not found.");
-            //}
+            var userClaims = HttpContext.User.Claims;
+            var userTypeClaim = HttpContext.User.Claims.FirstOrDefault(c => c.Type == "type")?.Value;
+            if (userTypeClaim == null)
+            {
+                return Unauthorized("User Type claim not found.");
+            }
 
-            //if (userTypeClaim != "octa") 
-            //{
-            //    return Unauthorized("Access Denied");
-            //}
+            if (userTypeClaim != "octa")
+            {
+                return Unauthorized("Access Denied");
+            }
 
             List<Domain> Domains = _Unit_Of_Work.domain_Octa_Repository.Select_All_Octa();
 

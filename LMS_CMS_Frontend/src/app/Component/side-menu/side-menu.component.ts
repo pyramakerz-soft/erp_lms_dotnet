0cc512dea4f713589b8a299f037e4fe686896a41
= <=>
2024-12-05 13:05:24 +0200

    search icon in navbar

--- LMS_CMS_Frontend/src/app/Component/side-menu/side-menu.component.ts
+++ LMS_CMS_Frontend/src/app/Component/side-menu/side-menu.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { PagesWithRoleId } from '../../Models/pages-with-role-id';
 import { SideMenuItemComponent } from '../side-menu-item/side-menu-item.component';
@@ -16,10 +16,33 @@ export class SideMenuComponent {
   @Input() menuItems?: { label: string; route?: string;  subItems?: { label: string; route: string }[]}[] = [];
   @Input() menuItemsForEmployee?: PagesWithRoleId[];
 
+  @ViewChild('searchInput') searchInput: any;
+  
   IsMenuOpen = false
+  IsSearchFocus = false
+
   constructor(private communicationService: NewTokenService) {}
 
   toggleMenu() {
+    console.log(this.menuItems)
     this.IsMenuOpen = !this.IsMenuOpen
+    this.IsSearchFocus = false
+  }
+
+  toggleSearch() {
+    this.IsMenuOpen = !this.IsMenuOpen
+    this.IsSearchFocus = !this.IsSearchFocus;
+  }
+
+  focusSearchInput() {
+    if (this.searchInput) {
+      this.searchInput.nativeElement.focus();
+    }
+  }
+
+  ngAfterViewChecked() {
+    if (this.IsSearchFocus) {
+      this.focusSearchInput();
+    }
   }
 }

ce7048d57d696a73aeed90ca1f9ccd1a09074536
= <=>
2024-12-22 08:45:28 +0200

    Octa Account

---
+++ LMS_CMS_Backend/LMS_CMS_DAL/Migrations/Octa/20241222061739_ArabicNameInOctaMigration.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS_CMS_DAL.Migrations.Octa
+{
+    /// <inheritdoc />
+    public partial class ArabicNameInOctaMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Octa_Email",
+                table: "Octa");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Octa");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Arabic_Name",
+                table: "Octa",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Arabic_Name",
+                table: "Octa");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Octa",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Octa_Email",
+                table: "Octa",
+                column: "Email",
+                unique: true);
+        }
+    }
+}
--- LMS_CMS_Backend/LMS_CMS_DAL/Migrations/Octa/20241222061954_CreateOctaAccMigration.cs
+++ LMS_CMS_Backend/LMS_CMS_DAL/Migrations/Octa/20241222061954_CreateOctaAccMigration.cs
@@ -5,13 +5,13 @@
 namespace LMS_CMS_DAL.Migrations.Octa
 {
     /// <inheritdoc />
-    public partial class CreateOctaAccountMigration : Migration
+    public partial class CreateOctaAccMigration : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql(@"
-                INSERT INTO Octa(User_Name, Password, Email) VALUES('Octa', 'Octa2019@pass', 'Octa@octa.com')
+                INSERT INTO Octa(User_Name, Arabic_Name, Password) VALUES('Octa', N'أوكتا', 'Octa2019@pass')
             ");
         }
 

e02a801ff81b7df91a22d56e0575a842a1e03bc1
= <=>
2024-12-22 09:16:22 +0200

    Emp

--- LMS_CMS_Backend/LMS_CMS_DAL/Models/Domains/EmployeeAttachment.cs
+++ LMS_CMS_Backend/LMS_CMS_DAL/Models/Domains/EmployeeAttachment.cs
@@ -1,21 +1,24 @@
-﻿using LMS_CMS_DAL.Models.Domains.Violations;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LMS_CMS_DAL.Models.Domains
 {
-    public class EmployeeType
+    public class EmployeeAttachment
     {
         [Key]
         public long ID { get; set; }
         [Required(ErrorMessage = "Name is required")]
         [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters.")]
         public string Name { get; set; }
-        public ICollection<Employee> Employees { get; set; } = new HashSet<Employee>();
-        public ICollection<EmployeeTypeViolation> EmployeeTypeViolations { get; set; } = new HashSet<EmployeeTypeViolation>();
+        public string Link { get; set; }
+        [ForeignKey("Employee")]
+        public long EmployeeID { get; set; }
+
+        public Employee Employee { get; set; }
     }
 }
--- LMS_CMS_Backend/LMS_CMS_DAL/Models/Domains/Violations/Violations.cs
+++ LMS_CMS_Backend/LMS_CMS_DAL/Models/Domains/Violations/Violations.cs
@@ -1,21 +1,19 @@
-﻿using LMS_CMS_DAL.Models.Domains.Violations;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LMS_CMS_DAL.Models.Domains
+namespace LMS_CMS_DAL.Models.Domains.Violations
 {
-    public class EmployeeType
+    public class Violations
     {
         [Key]
         public long ID { get; set; }
         [Required(ErrorMessage = "Name is required")]
         [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters.")]
         public string Name { get; set; }
-        public ICollection<Employee> Employees { get; set; } = new HashSet<Employee>();
         public ICollection<EmployeeTypeViolation> EmployeeTypeViolations { get; set; } = new HashSet<EmployeeTypeViolation>();
     }
 }

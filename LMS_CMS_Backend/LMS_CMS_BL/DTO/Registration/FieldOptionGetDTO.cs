8d71861fec268d29ae2ddd4c865cff4c23abd972
= <=>
2025-01-09 17:23:31 +0200

    DTO

---
+++ LMS_CMS_Backend/LMS_CMS_BL/DTO/Registration/FieldOptionGetDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS_CMS_BL.DTO.Registration
+{
+    public class FieldOptionGetDTO
+    {
+        public long ID { get; set; }
+        public string Name { get; set; }
+        public long CategoryFieldID { get; set; }
+        public string CategoryFieldName { get; set; }
+    }
+}

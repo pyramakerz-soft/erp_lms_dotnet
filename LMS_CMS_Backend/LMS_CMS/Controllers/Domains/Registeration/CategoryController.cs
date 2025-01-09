ea512f503acf20a06e1cdfdaa24cf19126980f01
mennausama <mennausama1682@gmail.com>
2025-01-09 15:21:14 +0200

    Registration in dto

--- LMS_CMS_Backend/LMS_CMS/Controllers/Domains/Registeration/CategoryController.cs
+++ LMS_CMS_Backend/LMS_CMS/Controllers/Domains/Registeration/CategoryController.cs
@@ -1,11 +1,49 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using LMS_CMS_BL.DTO.LMS;
+using LMS_CMS_BL.UOW;
+using LMS_CMS_DAL.Models.Domains.RegisterationModule;
+using LMS_CMS_PL.Attribute;
+using LMS_CMS_PL.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LMS_CMS_PL.Controllers.Domains.Registeration
 {
-    [Route("api/[controller]")]
+    [Route("api/with-domain/[controller]")]
     [ApiController]
+    [Authorize]
     public class CategoryController : ControllerBase
     {
+        private readonly DbContextFactoryService _dbContextFactory;
+        IMapper mapper;
+
+        public CategoryController(DbContextFactoryService dbContextFactory, IMapper mapper)
+        {
+            _dbContextFactory = dbContextFactory;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        [Authorize_Endpoint_(
+         allowedTypes: new[] { "octa", "employee" },
+         pages: new[] { "Registration Form Field", "Registration" }
+         )]
+        public async Task<IActionResult> GetAsync()
+        {
+            UOW Unit_Of_Work = _dbContextFactory.CreateOneDbContext(HttpContext);
+
+            List<RegistrationCategory> categories = await Unit_Of_Work.registrationCategory_Repository.Select_All_With_IncludesById<RegistrationCategory>(
+                    b => b.IsDeleted != true);
+
+            if (categories == null || categories.Count == 0)
+            {
+                return NotFound();
+            }
+
+            List<BuildingGetDTO> BuildingDTO = mapper.Map<List<BuildingGetDTO>>(categories);
+
+            return Ok(BuildingDTO);
+        }
     }
 }

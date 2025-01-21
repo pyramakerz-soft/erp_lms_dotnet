795a4c7bd3fbaccb3eb385b26b1ff1b97ed915ab
menna osama <menna osama@gmail.com>
2025-01-22 00:22:37 +0200

    front accounting 1

---
+++ LMS_CMS_Frontend/src/app/Models/Accounting/outcome.ts
@@ -0,0 +1,9 @@
+export class Outcome {
+    constructor(
+        public id: number = 0,
+        public name: string = '',
+        public accountNumberId :number =0,
+        public insertedByUserId :number =0,
+        public insertedAt :number =0,
+    ) {}
+}

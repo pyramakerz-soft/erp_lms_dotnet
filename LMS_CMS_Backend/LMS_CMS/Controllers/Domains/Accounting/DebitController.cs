04ddc96b75d49ef4ced198210e1d2a3f34726110
mennausama <mennausama1682@gmail.com>
2025-01-22 15:07:21 +0200

    Accounting folder

---
+++ LMS_CMS_Backend/LMS_CMS/Controllers/Domains/Accounting/DebitController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS_CMS_PL.Controllers.Domains.Accounting
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+    }
+}

0a55fc69942cd5210318d13d6e694c0daa0edbea
Seif-shawky <bsyco006@gmail.com>
2025-02-28 18:02:40 +0200

    test

--- LMS_CMS_Frontend/src/app/app.routes.ts
+++ LMS_CMS_Frontend/src/app/app.routes.ts
@@ -100,18 +100,18 @@ import { ShopItemsAddEditComponent } from './Pages/Employee/Inventory/shop-items
 import { StoresComponent } from './Pages/Employee/Inventory/stores/stores.component';
 import { SalesComponent } from './Pages/Employee/Inventory/sales/sales.component';
 import { SalesItemComponent } from './Pages/Employee/Inventory/sales-item/sales-item.component';
-import { HygieneTypesComponent } from './Pages/Employee/Clinic/hygiene-types/hygiene-types.component';
-import { DiagnosisComponent } from './Pages/Employee/Clinic/diagnosis/diagnosis.component';
-import { DrugsComponent } from './Pages/Employee/Clinic/drugs/drugs.component';
-import { HygieneFormComponent } from './Pages/Employee/Clinic/hygiene-form/hygiene-form.component';
-import { CreateHygieneFormComponent } from './Pages/Employee/Clinic/create-hygiene-form/create-hygiene-form.component';
 import { InventoryMasterComponent } from './Pages/Employee/Inventory/inventory-master/inventory-master.component';
 import { InventoryDetailsComponent } from './Pages/Employee/Inventory/inventory-details/inventory-details.component';
+import { CreateHygieneFormComponent } from './Pages/Employee/Clinic/create-hygiene-form/create-hygiene-form.component';
+import { HygieneFormComponent } from './Pages/Employee/Clinic/hygiene-form/hygiene-form.component';
+import { DrugsComponent } from './Pages/Employee/Clinic/drugs/drugs.component';
+import { DiagnosisComponent } from './Pages/Employee/Clinic/diagnosis/diagnosis.component';
+import { HygieneTypesComponent } from './Pages/Employee/Clinic/hygiene-types/hygiene-types.component';
 
 export const routes: Routes = [
-    { path: "", component: LoginComponent, title: "Login", canActivate:[] },
-    { path: "Octa/login", component: OctaLoginComponent, title: "login", canActivate:[] },
-    { path: "SignUp", component: SignUpComponent, title: "SignUp", canActivate:[] },
+    { path: "", component: LoginComponent, title: "Login", canActivate:[noNavigateToLoginIfLoginGuard] },
+    { path: "Octa/login", component: OctaLoginComponent, title: "login", canActivate:[noNavigateToLoginIfLoginGuard] },
+    { path: "SignUp", component: SignUpComponent, title: "SignUp", canActivate:[noNavigateToLoginIfLoginGuard] },
 
     
     { 
@@ -125,6 +125,7 @@ export const routes: Routes = [
             { path: "Drugs", component: DrugsComponent, title: "Drugs" },
             { path: "Hygiene Form Medical Report", component: HygieneFormComponent, title: "Hygiene Form" },
             { path: "Create Hygiene Form", component: CreateHygieneFormComponent, title: "Create Hygiene Form" },
+
             { path: "", component: EmployeeHomeComponent, title: "EmployeeHome" }, 
             { path: "Bus Details",component: BusDetailsComponent,title: "Bus"  , canActivate:[noNavigateWithoutLoginGuard, navigateIfHaveSettingPageGuard]},     
             { path: "Bus Students/:domainName/:busId", component: BusStudentComponent, title: "Bus Students", canActivate:[noNavigateWithoutLoginGuard, navigateIfHaveSettingPageGuard]}, 
@@ -265,4 +266,3 @@ export const routes: Routes = [
 
     { path: '**', redirectTo: '/' }
 ];
-

5cc20d5ce33885f9614a6117e5c38e18dfb268b3
mennausama <mennausama1682@gmail.com>
2025-03-06 12:55:26 +0200

    PurchaseOrder

---
+++ LMS_CMS_Backend/LMS_CMS_DAL/Models/Domains/Inventory/stocking.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS_CMS_DAL.Models.Domains.Inventory
+{
+    internal class stocking
+    {
+    }
+}

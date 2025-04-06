24551fe24f7e0b5c582d3a44e42db872aad568a9
= <=>
2025-04-06 12:09:14 +0200

    Domain From Link

--- LMS_CMS_Frontend/src/app/Services/api.service.ts
+++ LMS_CMS_Frontend/src/app/Services/api.service.ts
@@ -15,8 +15,10 @@ export class ApiService {
   BaseUrlOcta="http://44.210.155.226:5000/api"
   constructor() { }
 
-  GetHeader(){   
-    var Header = "Domain_One"     
+  GetHeader(){    
+    const hostname = window.location.hostname;  
+    var Header = hostname.split('.')[0] 
+    // var Header = "Domain_Two"      
     return Header;
   }
 }

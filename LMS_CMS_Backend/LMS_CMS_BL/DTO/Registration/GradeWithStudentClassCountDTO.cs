de2160c2d06659e3c5a89555b1a5054b1f5a78ac
= <=>
2025-04-08 16:47:42 +0200

    Grade

---
+++ LMS_CMS_Backend/LMS_CMS_BL/DTO/Registration/GradeWithStudentClassCountDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS_CMS_BL.DTO.Registration
+{
+    public class GradeWithStudentClassCountDTO
+    {
+        public long ID { get; set; } 
+        public string Name { get; set; }
+        public int ClassCount { get; set; }
+        public int SaudiCount { get; set; }
+        public int NonSaudiCount { get; set; }
+        public int StudentCount { get; set; }
+        public int StudentsAssignedToNoorCount { get; set; }
+    }
+}

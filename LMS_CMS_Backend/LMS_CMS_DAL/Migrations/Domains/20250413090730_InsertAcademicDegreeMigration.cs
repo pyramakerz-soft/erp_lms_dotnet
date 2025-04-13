aa43827d091d017974e44e282dea7162aa0059a7
= <=>
2025-04-13 14:00:26 +0200

    Migration

---
+++ LMS_CMS_Backend/LMS_CMS_DAL/Migrations/Domains/20250413090730_InsertAcademicDegreeMigration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS_CMS_DAL.Migrations.Domains
+{
+    /// <inheritdoc />
+    public partial class InsertAcademicDegreeMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                INSERT INTO AcademicDegrees (ID, Name) VALUES (1, 'Bachelor');
+                INSERT INTO AcademicDegrees (ID, Name) VALUES (2, 'Master');
+                INSERT INTO AcademicDegrees (ID, Name) VALUES (3, 'Doctorate');
+                INSERT INTO AcademicDegrees (ID, Name) VALUES (4, 'Associate');
+                INSERT INTO AcademicDegrees (ID, Name) VALUES (5, 'Diploma');
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DELETE FROM AcademicDegrees  WHERE ID IN (1, 2, 3, 4, 5);
+            ");
+        }
+    }
+}

433368e5d49b6f6c12a27466f2cc146f2c576967
asalem.dev@gmail.com <asalem.dev@gmail.com>
2025-04-14 14:21:36 +0200

    update hygiene form and student

---
+++ LMS_CMS_Backend/LMS_CMS_DAL/Models/Domains/ClinicModule/StudentHygiens.cs
@@ -0,0 +1,9 @@
+﻿namespace LMS_CMS_DAL.Models.Domains.ClinicModule
+{
+    public class StudentHygiens : AuditableEntity
+    {
+        public long ID { get; set; }
+        public long StudentId { get; set; }
+        public long HygieneTypeId { get; set; }
+    }
+}

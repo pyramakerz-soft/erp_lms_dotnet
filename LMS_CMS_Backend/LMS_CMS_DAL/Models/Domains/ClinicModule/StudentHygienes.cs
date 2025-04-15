1c97f98f459820d25b4d164f059268f027613a21
asalem.dev@gmail.com <asalem.dev@gmail.com>
2025-04-15 14:31:02 +0200

    update hygiene forms and zatca

--- LMS_CMS_Backend/LMS_CMS_DAL/Models/Domains/ClinicModule/StudentHygienes.cs
+++ LMS_CMS_Backend/LMS_CMS_DAL/Models/Domains/ClinicModule/StudentHygienes.cs
@@ -1,6 +1,6 @@
 ﻿namespace LMS_CMS_DAL.Models.Domains.ClinicModule
 {
-    public class StudentHygiens : AuditableEntity
+    public class StudentHygienes : AuditableEntity
     {
         public long ID { get; set; }
         public long StudentId { get; set; }

695a86d7f0f17b0191e1fb4357ce6303dc89c2b3
= <=>
2025-04-23 15:41:27 +0200

    Migrations

---
+++ LMS_CMS_Backend/LMS_CMS_DAL/Models/Domains/LMS/EvaluationEmployeeStudentBookCorrection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS_CMS_DAL.Models.Domains.LMS
+{
+    public class EvaluationEmployeeStudentBookCorrection : AuditableEntity
+    {
+        [Key]
+        public long ID { get; set; }
+        public int State { get; set; }
+        public string? Note { get; set; }
+
+        [ForeignKey("EvaluationEmployee")]
+        public long EvaluationEmployeeID { get; set; }
+        public EvaluationEmployee EvaluationEmployee { get; set; }
+
+        [ForeignKey("EvaluationBookCorrection")]
+        public long EvaluationBookCorrectionID { get; set; }
+        public EvaluationBookCorrection EvaluationBookCorrection { get; set; }
+
+        [ForeignKey("Student")]
+        public long StudentID { get; set; }
+        public Student Student { get; set; } 
+    }
+}
---
+++ LMS_CMS_Backend/LMS_CMS_DAL/Models/Domains/LMS/EvaluationTemplateGroupQuestion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS_CMS_DAL.Models.Domains.LMS
+{
+    public class EvaluationTemplateGroupQuestion : AuditableEntity
+    {
+        [Key]
+        public long ID { get; set; }
+        [Required(ErrorMessage = "English Title is required")]
+        public string EnglishTitle { get; set; }
+        [Required(ErrorMessage = "Arabic Title is required")]
+        public string ArabicTitle { get; set; }
+        public int Mark { get; set; }
+
+        [ForeignKey("EvaluationTemplateGroup")]
+        public long EvaluationTemplateGroupID { get; set; }
+        public EvaluationTemplateGroup EvaluationTemplateGroup { get; set; }
+
+        public ICollection<EvaluationEmployeeQuestion> EvaluationEmployeeQuestions { get; set; } = new HashSet<EvaluationEmployeeQuestion>();
+    }
+}

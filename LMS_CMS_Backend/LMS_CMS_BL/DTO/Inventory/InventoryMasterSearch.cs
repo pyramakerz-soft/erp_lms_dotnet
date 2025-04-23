ea4a9d0eef5dcae5cd23429e3f73a9499b050abf
mennausama <mennausama1682@gmail.com>
2025-04-23 09:58:07 +0200

    inventory search

---
+++ LMS_CMS_Backend/LMS_CMS_BL/DTO/Inventory/InventoryMasterSearch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS_CMS_BL.DTO.Inventory
+{
+    public class InventoryMasterSearch
+    {
+        public long storeId { get; set; }
+        public string DateFrom { get; set; }
+        public string DateTo { get; set; }
+        public List<long> FlagIds { get; set; }
+        //public List<long>? ShopItemsIds { get; set; }
+
+    }
+}

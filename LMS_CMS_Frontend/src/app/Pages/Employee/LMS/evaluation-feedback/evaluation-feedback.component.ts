67b87dddb504b710f39fda9468e057e9d14149e7
mennausama <mennausama1682@gmail.com>
2025-04-27 12:27:50 +0300

    feedback

---
+++ LMS_CMS_Frontend/src/app/Pages/Employee/LMS/evaluation-feedback/evaluation-feedback.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-evaluation-feedback',
+  standalone: true,
+  imports: [],
+  templateUrl: './evaluation-feedback.component.html',
+  styleUrl: './evaluation-feedback.component.css'
+})
+export class EvaluationFeedbackComponent {
+
+}

6952112674127fa9b455272e9144ac2c57ee579e
mennausama <mennausama1682@gmail.com>
2025-04-28 12:55:32 +0300

    inventory flag

---
+++ LMS_CMS_Backend/LMS_CMS_DAL/Migrations/Domains/20250428095227_FlagInventory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS_CMS_DAL.Migrations.Domains
+{
+    /// <inheritdoc />
+    public partial class FlagInventory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE InventoryFlags SET enName = 'Transfer to Store' WHERE ID = 8");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE InventoryFlags SET enName = 'Transfer to Warehouse' WHERE ID = 8");
+
+        }
+    }
+}
